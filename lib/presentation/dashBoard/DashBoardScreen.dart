e062f23b1323632ad32a2edd3ad558aeab66b166
moham <fahmyabada@gmail.com>
2022-09-27 15:55:59 +0200

    27-9-2022   from work

--- lib/presentation/dashBoard/DashBoardScreen.dart
+++ lib/presentation/dashBoard/DashBoardScreen.dart
@@ -7,11 +7,42 @@ class DashBoardScreen extends StatefulWidget {
   State<DashBoardScreen> createState() => _DashBoardScreenState();
 }
 
-class _DashBoardScreenState extends State<DashBoardScreen> {
+class _DashBoardScreenState extends State<DashBoardScreen> with SingleTickerProviderStateMixin{
+  int _currentTab = 0;
+  List<Widget> _tabList = [];
+  TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: length, vsync: this)
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('dashboard'),),
+      body: TabBarView(
+        controller: ,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _currentTab = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: "current request"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "upcoming request"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_pin_outlined), label: "past request"),
+        ],
+      ),
     );
   }
 }
